1f9ffa7225e178fb889c228342cf9d448ff2b48c
Alexandre Ramirez <ps78ppi@eduvaud.ch>
2024-12-20 12:02:40 +0100

    feat :class MathRequest

--- src/MathLib.py
+++ src/MathLib.py
@@ -1,22 +1,28 @@
+from src.MathRequest import MathRequest
+
+
 class MathLib:
 
     @classmethod
     def execute(cls, math_request):
-        operator = None
+        operator = math_request.get_operator()
+        ope1 = math_request.get_ope1()
+        ope2 = math_request.get_ope2()
 
         match operator:
             case 'add':
-                raise NotImplementedError
+                resultat = ope1 + ope2
+                math_request.set_res(resultat)
             case 'sub':
-                raise NotImplementedError
+                math_request.set_res(ope1 - ope2)
             case 'mul':
-                raise NotImplementedError
+                math_request.set_res(ope1 * ope2)
             case 'div':
-                raise NotImplementedError
+                math_request.set_res(ope1 / ope2)
             case 'pow':
-                raise NotImplementedError
+                math_request.set_res(ope1 ** ope2)
             case 'root':
-                raise NotImplementedError
+                math_request.set_res(ope1 % ope2)
             case _:
                 raise NotImplementedError
 
